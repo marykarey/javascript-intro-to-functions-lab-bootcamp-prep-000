d7d02db9dd817613a1c87b9f062f244edf0d613a
Mary <kiarie.mary89@gmail.com>
2018-04-18 19:33:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string === lowercase{
+  if string.lowercase === lowercase{
     return ("I can\'t hear you!")
   }
-  else if string === uppercase{
+  else if string.uppercase === uppercase{
     return ("YES INDEED!")
   }else if string === "I love you, Grandma."{
     return ("I love you, too.")
